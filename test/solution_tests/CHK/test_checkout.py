6cbcb852d17bf77cc516c115d76cbdf619f15917
sbx_user1051 <sbx_user1051@169.254.1.253>
2022-05-05 13:01:09 +0000

    Thu May 05 13:01:09 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,6 +3,14 @@ from solutions.CHK.checkout_solution import checkout
 
 class TestCheckout:
 
-    def test_checkout(self):
-        assert False
+    def test_checkout_illegal_input(self):
+        illegal_string_response = "1, 2, 3"
+        illegal_non_string_response = ["A, B, C"]
+
+        response_1 = checkout(illegal_string_response)
+        response_2 = checkout(illegal_non_string_response)
+
+        assert response_1 == -1
+        assert response_2 == -1
+
 
