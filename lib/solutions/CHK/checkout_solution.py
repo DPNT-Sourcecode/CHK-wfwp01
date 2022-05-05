8cc3f3932b4728ec6b62151b1f9a6a6b4153e677
sbx_user1051 <sbx_user1051@169.254.1.253>
2022-05-05 13:10:09 +0000

    Thu May 05 13:10:09 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,11 +2,17 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+from curses.ascii import isalpha
+
+
 def checkout(skus):
     if not isinstance(skus, str):
         return -1
-    sku_list = skus.split(",")
-    if not all([isalpha(sku) for sku in sku_list]):
+    sku_list = skus.split(",").strip(" ")
+    if all([isalpha(sku) for sku in sku_list]):
+        pass
+    else:
         return -1
+
 
 
