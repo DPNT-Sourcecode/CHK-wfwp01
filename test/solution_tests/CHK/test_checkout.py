9eb356617b72570a422ce67216d5c2c2fbc58add
sbx_user1051 <sbx_user1051@169.254.1.253>
2022-05-05 16:18:09 +0000

    Thu May 05 16:18:09 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -43,6 +43,7 @@ class TestCheckout:
         assert total == 195
 
     def test_checkout_with_multiple_special_offers(self):
-        skus = "A, A, A, A, A, A, B, C, D"
+        skus = "A, A, A, A, A, B, C, D"
         total = checkout(skus)
-        assert total == 325
+        assert total == 365
+
